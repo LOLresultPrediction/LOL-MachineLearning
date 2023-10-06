be876e4ca3bb7c48b8e2578533f424678e29c018
wooyoung <rnjsdndud00@naver.com>
2023-10-06 21:34:04 +0900

    FIX. FileName

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 if __name__ == "__main__":
-    # pp.pprint(getTempDataset.tempResult('KR_6708057039', 10))
-    pp.pprint(getDataset.getDataSet('KR_6709504031', 10))
+    pp.pprint(getTempDataset.tempResult('KR_6708057039', 15))
+    pp.pprint(getDataset.getDataSet('KR_6708057039', 15))
     # pp.pprint(getAPI.getGameInfo('KR_6708057039')['info']['teams'])
+
 # pp.pprint(getGameInfo('KR_6708057039')['info']['participants'][0])
 # pp.pprint(getGameInfoTimeline('KR_6709531155').keys())
 # pp.pprint(getGameInfoTimeline('KR_6708057039')['info']['frames'][10]['events'])
