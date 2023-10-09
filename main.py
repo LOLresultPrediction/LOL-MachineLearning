1c5fbc20ecbbc872631778b8dc0f83d903d5b10e
wooyoung <rnjsdndud00@naver.com>
2023-10-09 21:05:22 +0900

    FIX. winTeam dataset

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pprint
 import numpy as np
 import getAPI
-import getTempDataset
+import getRealDataset
 import getDataset
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -9,12 +9,27 @@ pp = pprint.PrettyPrinter(indent=4)
 
 # pp.pprint(getGameInfo('KR_6708057039')['info']['teams'][0]['objectives'])
 
-
+'''
+'KR_6710383118', 1. 브라이어(5/14/5)
+'KR_6709906475', 2. 브라이어(7/9/1)
+'KR_6709583655', 3. 브라이어(0/0/0)
+'KR_6709531155', 4. 나피리(8/9/2)
+'KR_6709504031', 5. 라이즈
+'KR_6708057039', 6. 브라이어(0/9/0)
+'KR_6708035868',
+'KR_6707425375',
+'KR_6707289771',
+'KR_6704954051']
+'''
 if __name__ == "__main__":
-    pp.pprint(getTempDataset.tempResult('KR_6708057039', 15))
-    pp.pprint(getDataset.getDataSet('KR_6708057039', 15))
-    # pp.pprint(getAPI.getGameInfo('KR_6708057039')['info']['teams'])
+    pp.pprint(getRealDataset.tempResult('KR_6704954051', 16))
+    # gameTimelineInfo = getAPI.getGameInfoTimeline('KR_6708057039')
+    # gameInfo = getAPI.getGameInfo('KR_6709906475')
+    # for i in range(10):
+    #     pp.pprint(gameInfo['info']['participants'][i]['participantId'])
+    #     pp.pprint(gameInfo['info']['participants'][i]['summonerName'])
+    #     pp.pprint(gameInfo['info']['participants'][i]['championName'])
+    # pp.pprint(gameInfo['info']['teams'][1]['objectives'])
 
-# pp.pprint(getGameInfo('KR_6708057039')['info']['participants'][0])
-# pp.pprint(getGameInfoTimeline('KR_6709531155').keys())
-# pp.pprint(getGameInfoTimeline('KR_6708057039')['info']['frames'][10]['events'])
+    # pp.pprint(getDataset.getDataSet('KR_6708057039', 15))
+    # pp.pprint(getAPI.getGameInfo('KR_6708057039')['info']['teams'])
