0ad79f49be8a162e1bffc1227d97128813a8f822
김재범 <process68@naver.com>
2023-10-14 12:02:51 +0900

    1014

--- getRealDataset.py
+++ getRealDataset.py
@@ -3,6 +3,7 @@ import numpy as np
 import getAPI
 import etcFunction as ef
 import csv
+import getMatchId
 pp = pprint.PrettyPrinter(indent=4)
 
 '''
@@ -12,13 +13,10 @@ pp = pprint.PrettyPrinter(indent=4)
 1분 30초 이전에 킬이 발생했는지 = bool
 
 '''
-def saveDataSetToCSV(matchId, frame):
-    dataset = tempResult(matchId, frame) 
-    
-    dataset['matchId'] = matchId #매치 아이디 값을 불러오기 위한 작업
 
-   
-    csv_file_name = f"{matchId}_frame{frame}_data.csv"
+def saveDataSetToCSV(matchIdSet, frame):
+    
+    csv_file_name = f"bronze_frame_data.csv"
 
     #딕셔너리 키, 값 형태로 엑셀에 불러옴. (키값이 엑셀의 행이되고 열이 딕셔나리의 값이 오게된다.)
     fieldnames = [
@@ -42,10 +40,15 @@ def saveDataSetToCSV(matchId, frame):
     # 저장하는 로직
     with open(csv_file_name, mode='w', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        
+        
+        
+        
+        
 
         writer.writeheader()
-
-        writer.writerow(dataset)
+        for i in range(2):
+            writer.writerow(dataset)
 
 
 # 15분 후 게임 데이터 셋
@@ -162,6 +165,7 @@ def tempResult(matchId, frame):
             loseTeamValue['minionsKilled'].append(participantFrames['minionsKilled'])
             loseTeamValue['jungleMinionsKilled'].append(participantFrames['jungleMinionsKilled'])
 
+    dataSet['matchId'] = matchId
     dataSet['Diff_LV'] = sum(np.array(winTeamValue['level']) - np.array(loseTeamValue['level']))
     dataSet['Diff_CS'] = sum(np.array(winTeamValue['minionsKilled']) - np.array(loseTeamValue['minionsKilled']))
     dataSet['Diff_jglCS'] = sum(np.array(winTeamValue['jungleMinionsKilled']) - np.array(loseTeamValue['jungleMinionsKilled']))
@@ -174,13 +178,14 @@ def tempResult(matchId, frame):
     dataSet['Diff_WARDkill'] = len(winTeamValue['wardKillerId']) - len(loseTeamValue['wardKillerId'])
     dataSet['Diff_Inhibitor'] = len(winTeamValue['inhibitorBreakerId']) - len(loseTeamValue['inhibitorBreakerId'])
     dataSet['result'] = 1
+    
     # pp.pprint(winTeamValue)
     # pp.pprint(loseTeamValue)
     
     return dataSet
 
 
-dataSet= tempResult("KR_6710383118", 15)
+dataSet= tempResult("KR_6721548861", 15)
 pp.pprint(dataSet)
 
-saveDataSetToCSV("KR_6710383118", 15)
+saveDataSetToCSV("KR_6721548861", 15)
