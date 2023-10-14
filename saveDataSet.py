3ea578536869a3712ab4ec4ac79679065e65d12c
Your Name <you@example.com>
2023-10-14 14:12:15 +0900

    주석 제거

--- saveDataSet.py
+++ saveDataSet.py
@@ -2,7 +2,23 @@ import getDataset
 import csv
 
 def saveDataSetToCSV(matchIdSet, fileName, frame):
-    fieldnames = getDataset.tempResult('KR_6714278507', 15)
+    fieldnames = [
+        'matchId',
+        'Diff-A',
+        'Diff-K',
+        'Diff_CS',
+        'Diff_FirstBLOOD',
+        'Diff_FirstDRAGON',
+        'Diff_FirstHERALD',
+        'Diff_Firsttower',
+        'Diff_Inhibitor',
+        'Diff_LV',
+        'Diff_WARDkill',
+        'Diff_WARDplaced',
+        'Diff_jglCS',
+        'dragonType',
+        'result'
+    ]
     with open(fileName,'w', newline='') as f:
         w = csv.DictWriter(f, fieldnames.keys())
         w.writeheader()
